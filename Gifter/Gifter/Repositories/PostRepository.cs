fe88928192240e33da25799d68e592d69b1baf03
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-11 21:49:14 -0600

    fully CRUD on all Post

--- Gifter/Gifter/Repositories/PostRepository.cs
+++ Gifter/Gifter/Repositories/PostRepository.cs
@@ -32,5 +32,24 @@ namespace Gifter.Repositories
                             .OrderBy(p => p.Title)
                             .ToList();
         }
+
+        public void AddPost(Post post)
+        {
+            _context.Add(post);
+            _context.SaveChanges();
+        }
+
+        public void Update(Post post)
+        {
+            _context.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var post = GetById(id);
+            _context.Post.Remove(post);
+            _context.SaveChanges();
+        }
     }
 }
