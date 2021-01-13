3c21218b25f4bbaa73521482a9a0012808ca74c0
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-13 11:22:05 -0600

    posts are completely wired up to display on react

--- Gifter/Gifter/client/src/components/PostList.js
+++ Gifter/Gifter/client/src/components/PostList.js
@@ -1,33 +1,27 @@
-import React, { UseEffect, UseState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PostList = () => {
-    const [posts, setPosts] = UseState([]);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch('/api/posts') //this is just an HTTP request to the api
-            .then(res => res.json) //request the json data found on api/posts
-            .then(data => setPosts(data)); //takes the json data received, and sets the posts state
-    }, [])
-
-    //Once the useEffect is complete, use the info to display
+        fetch('/api/post')
+            .then(res => res.json())
+            .then(data => setPosts(data));
+    }, []);
 
     return (
         <div>
             {posts.map((post) => (
                 <div key={post.id}>
-                    <img>{post.imageUrl}</img>
+                    <img src={post.imageUrl} alt={post.title} />
                     <p>
                         <strong>{post.title}</strong>
                     </p>
                     <p>{post.caption}</p>
                 </div>
-            )
-            )}
+            ))}
         </div>
     );
-
 };
 
 export default PostList;
-//setting as default takes away needing to import with curly braces{}
-
