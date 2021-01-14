72a319ef8f43efbc08d3407e0ccbb51075935f50
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-14 14:27:50 -0600

    updated form to rely of state for adding post, connection made, just need to add all fields

--- Gifter/Gifter/client/src/components/PostForm.js
+++ Gifter/Gifter/client/src/components/PostForm.js
@@ -1,20 +1,55 @@
 import React, { useContext, useEffect, useState } from "react";
 
 const PostForm = () => {
-    const [post, setPost] = useState();
+    const [inputs, setInputs] = useState({
+        Title: "",
+        ImageUrl: "",
+        Caption: ""
+    });
 
     // useEffect(() => {
     //     fetch('api/')
     // })
 
-    const postData = () => {
-        fetch("api/post")
+    const handleChange = (event) => {
+        setInputs((inputs) => ({ ...inputs, [event.target.name]: event.target.value }));
     }
 
+    // const handleSubmit = (event) => {
+
+    //     alert(`Here we go!
+    //            Title is ${inputs.Title} `)
+    // }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs)
+        fetch('/api/post', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "Title": inputs.Title,
+                "ImageUrl": inputs.ImageUrl,
+                "Caption": inputs.Caption
+            })
+        }
+        )
+    }
+
+
     return (
-        <div>
-            <button onClick={() => postData()}>click me</button>
-        </div>
+        <form>
+            <label>Title</label>
+            <input type='text' name="Title" value={inputs.Title} onChange={handleChange} />
+            <label>Image Url</label>
+            <input type='text' name="ImageUrl" value={inputs.ImageUrl} onChange={handleChange} />
+            <label>Caption</label>
+            <input type='text' name="Caption" value={inputs.Caption} onChange={handleChange} />
+            <button type="submit" onClick={handleSubmit} >submit</button>
+
+        </form>
     )
 }
 
