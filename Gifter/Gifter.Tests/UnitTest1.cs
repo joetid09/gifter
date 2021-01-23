51788b8d2bfdeb4e1a2136a93e7ac0ef1955ec50
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-22 18:02:36 -0600

    test for equaling numbers now working

--- Gifter/Gifter.Tests/UnitTest1.cs
+++ Gifter/Gifter.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Gifter.Tests
         public void Two_Numbers_Should_Equal()
         {
             var num1 = 100;
-            var num2 = 200;
+            var num2 = 100;
 
             Assert.Equal(num1, num2);
         }
